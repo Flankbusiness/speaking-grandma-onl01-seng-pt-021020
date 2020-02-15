e52a45561efef6b49d55fcb0d4626ae17ca0745a
Steven Prindle <flankbusiness@gmail.com>
2020-02-15 20:06:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 def speak_to_grandma(phrase)
   
@@ -12,3 +13,17 @@ def speak_to_grandma(phrase)
     
   end
 end
+=======
+def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+  else phrase == "WHAT?"
+    return "NO, NOT SINCE 1938!"
+  end
+>>>>>>> c9ca7f580cae3f29f544cb34491e1c6e085fbe66
